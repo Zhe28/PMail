2888ab737bc91bff1dbe6210537e1b6d1d042efa
zhe28 <huangze28@foxmail.com>
2024-09-20 21:11:31 +0800

    feat: 代码分离，功能优化
    1. 代码迁移到 ts.
    2. 分离 axios 部分代码.
    3. 增加 pinia 支持，全局状态代码迁移到相对应的 store.
    4. 代码格式优化, 移除 ==.
    5. 代码声明更改，用 const 代替 var 声明.
    6. Header 使用 Router 导航.

--- fe/src/utils/axios.ts
+++ fe/src/utils/axios.ts
@@ -1,34 +1,20 @@
-import './assets/main.css'
-import 'element-plus/dist/index.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import {ref} from 'vue'
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-app.config.globalProperties.$isLogin = ref(true)
-app.config.globalProperties.$userInfos = ref({})
-app.use(createPinia())
-app.use(router)
-
-
-
 import axios from 'axios'
-import lang from './i18n/i18n';
-//创建axios的一个实例 
-var $http = axios.create({
+import lang from '../i18n/i18n';
+import {useGlobalStatusStore} from "../stores/useGlobalStatusStore";
+import {router} from "../router";
+
+//创建axios的一个实例
+const http = axios.create({
     baseURL: import.meta.env.VITE_APP_URL, //接口统一域名
     timeout: 60000, //设置超时
     headers: {
         'Content-Type': 'application/json;charset=UTF-8;',
         'Lang': lang.lang
     }
-})
+});
 
-//请求拦截器 
-$http.interceptors.request.use((config) => {
+//请求拦截器
+http.interceptors.request.use((config) => {
     //若请求方式为post，则将data参数转为JSON字符串
     if (config.method === 'POST') {
         config.data = JSON.stringify(config.data);
@@ -39,34 +25,34 @@ $http.interceptors.request.use((config) => {
     Promise.reject(error));
 
 //响应拦截器
-$http.interceptors.response.use((response) => {
+http.interceptors.response.use(async (response) => {
+    const globalStatus = useGlobalStatusStore();
     //响应成功
-    if (response.data.errorNo == 403) {
-        app.config.globalProperties.$isLogin.value = false
+    if (response.data.errorNo === 403) {
+        globalStatus.isLogin = false
 
-        router.replace({
+        await router.replace({
             path: '/login',
             query: {
                 redirect: router.currentRoute.fullPath
             }
-        })
+        });
     }
     //响应成功
     if (response.data.errorNo == 402) {
-        router.replace({
+        await router.replace({
             path: '/setup',
             query: {
                 redirect: router.currentRoute.fullPath
             }
-        })
+        });
     }
     return response.data;
-}, (error) => {
+}, async (error) => {
     //响应错误
     if (error.response && error.response.status) {
-        const status = error.response.status
         let message = ""
-        switch (status) {
+        switch (error.response.status) {
             case 400:
                 message = '请求错误';
                 break;
@@ -74,12 +60,12 @@ $http.interceptors.response.use((response) => {
                 message = '请求错误';
                 break;
             case 403:
-                router.replace({
+               await router.replace({
                     path: '/login',
                     query: {
                         redirect: router.currentRoute.fullPath
                     }
-                })
+                });
                 break;
             case 404:
                 message = '请求地址出错';
@@ -106,16 +92,12 @@ $http.interceptors.response.use((response) => {
                 message = 'HTTP版本不受支持';
                 break;
             default:
-                message = '请求失败'
+                // eslint-disable-next-line no-unused-vars
+                message = '请求失败';
         }
         return Promise.reject(error);
     }
     return Promise.reject(error);
 });
 
-
-// 注册到全局
-app.config.globalProperties.$http = $http
-
-app.mount('#app')
-
+export {http};
